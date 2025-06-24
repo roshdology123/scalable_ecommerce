5f8d50bb76ce08eaed090ce5071f3fe3e0a71811
Abdallah Swify <abdallahelswify@gmail.com>
2025-06-24 08:39:35 +0300

    feat: add search functionality with use cases and UI components; implement search history management and suggestions

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -16,6 +16,7 @@ android {
     compileOptions {
         sourceCompatibility = JavaVersion.VERSION_11
         targetCompatibility = JavaVersion.VERSION_11
+        isCoreLibraryDesugaringEnabled = true
     }
 
     kotlinOptions {
@@ -27,7 +28,7 @@ android {
         applicationId = "com.itg.scalable_ecommerce"
         // You can update the following values to match your application needs.
         // For more information, see: https://flutter.dev/to/review-gradle-config.
-        minSdk = flutter.minSdkVersion
+        minSdk = 23
         targetSdk = flutter.targetSdkVersion
         versionCode = flutter.versionCode
         versionName = flutter.versionName
@@ -44,4 +45,7 @@ android {
 
 flutter {
     source = "../.."
+}
+dependencies {
+    coreLibraryDesugaring("com.android.tools:desugar_jdk_libs:2.1.4")
 }
